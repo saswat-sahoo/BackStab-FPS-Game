be1aa93e3b27cb0dfce01cea412bc4da6f9338b6
saswat-sahoo <68820601+saswat-sahoo@users.noreply.github.com>
2021-08-19 00:00:45 +0530

    fixed gun movement bug

--- FPSgame/Assets/PlayerMovement.cs
+++ FPSgame/Assets/PlayerMovement.cs
@@ -5,28 +5,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
      public Rigidbody rb;
-    public float Force = 10f;
+     public float Force = 10f;
     
-    void Start()
-    {
-        
-    }
 
     void Update()
     {
     // basic movement for player
-    if(Input.GetKey("d")){
-    rb.AddForce(Force * Time.deltaTime, 0 , 0, ForceMode.VelocityChange);
-    }
-    if(Input.GetKey("a")){
-    rb.AddForce(-Force * Time.deltaTime, 0 , 0, ForceMode.VelocityChange);
-    }
-    if(Input.GetKey("w")){
-    rb.AddForce(0, 0 , Force * Time.deltaTime, ForceMode.VelocityChange);
-    }
-    if(Input.GetKey("s")){
-    rb.AddForce(0, 0 , -Force * Time.deltaTime, ForceMode.VelocityChange);
-    }
+        if(Input.GetKey("d")){
+        rb.AddForce(Force * Time.deltaTime, 0 , 0, ForceMode.VelocityChange);
+        }
+        if(Input.GetKey("a")){
+        rb.AddForce(-Force * Time.deltaTime, 0 , 0, ForceMode.VelocityChange);
+        }
+        if(Input.GetKey("w")){
+        rb.AddForce(0, 0 , Force * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        if(Input.GetKey("s")){
+        rb.AddForce(0, 0 , -Force * Time.deltaTime, ForceMode.VelocityChange);
+        }
 
    
     }
